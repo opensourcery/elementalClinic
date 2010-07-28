309e050b9e0c3788e649f5f3d99ee4e6c5c9ad54
Michael G. Schwern <schwern@pobox.com>
2010-07-27 23:57:01 -0700

    Fix deprecation warning from Moose::Util::MetaRole 1.09

--- app/lib/eleMentalClinic/Report/Labelled.pm
+++ app/lib/eleMentalClinic/Report/Labelled.pm
@@ -12,12 +12,26 @@ sub init_meta {
   my %p = @_;
   my $meta = Moose->init_meta(%p);
 
-  Moose::Util::MetaRole::apply_metaclass_roles(
-    for_class => $p{for_class},
-    attribute_metaclass_roles => [
-      'eleMentalClinic::Report::Meta::Attribute'
-    ],
-  );
+  # apply_metaclass_roles was deprecated in 0.94.
+  # Avoid a deprecation warning, but still work with Moose pre-0.94.
+  if( $Moose::Util::MetaRole::VERSION < 0.94 ) {
+      Moose::Util::MetaRole::apply_metaclass_roles(
+          for_class           => $p{for_class},
+          attribute_metaclass_roles => [
+              'eleMentalClinic::Report::Meta::Attribute'
+          ],
+      );
+  }
+  else {
+      Moose::Util::MetaRole::apply_metaroles(
+          for                 => $p{for} || $p{for_class},
+          class_metaroles     => {
+              attribute => [
+                  'eleMentalClinic::Report::Meta::Attribute'
+              ],
+          }
+      );
+  }
 }
 
 1;
